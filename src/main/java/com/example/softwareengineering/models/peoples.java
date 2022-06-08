18393d3dc96212356f557b420c804d31c3f547ab
Gotroenks <isaacmoura_gerboni@hotmail.com>
2022-06-08 11:17:18 -0300

    criação da model

---
+++ src/main/java/com/example/softwareengineering/models/peoples.java
@@ -0,0 +1,15 @@
+package com.example.softwareengineering.models;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
+public class peoples {
+
+    String name;
+    String profession;
+    int age;
+
+}
